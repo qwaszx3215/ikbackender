e0e7684a50f310818a162c77c2abd634c76225e3
qwaszx3215 <84395173+qwaszx3215@users.noreply.github.com>
2023-10-29 09:40:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ app.post("/sendmail5", async (req, res) => {
     });
     let info = await transporter.sendMail({
       from: '"Happy time" <info@korarahighlandstea.com>',
-      to: "fredrick3smith33@gmail.com",
+      to: "daniellahm@yandex.com",
       subject: "happy Time",
       html: `
     <div> 
